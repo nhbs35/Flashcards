7e30299eec30872fb14df184e1f4e76802f2cdf5
nhbs35 <00001111742364sp@al.educacao.sp.gov.br>
2024-11-01 13:36:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'O que são biosensores?',
+    'Dispositivos constituídos de um elemento de reconhecimento biológico (anticorpos, enzimas, proteínas, ácidos nucléicos, organelas e células), com a propriedade de reconhecer e interagir com o analito de interesse.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o resultado de 25% de 80?',
+    '25% de 80 é 20.'
+)
+
+criaCartao(
+    'Sociologia',
+    'Segundo Émile Durkheim (1858-1917), as três principais características do Fato Social são:?',
+    'A generalidade, a exterioridade e a coercitividade.'
+)
